5bef5f8f03180581814196eb7ac65bf1f4a38c8a
jaehoonchoi <eeoos8840@gmail.com>
2025-09-22 21:56:45 +0900

    refactor: JPQL로 구현되어 있던 로직을 QueryDSL로 변경

--- src/main/java/kr/kro/airbob/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/kr/kro/airbob/domain/reservation/repository/ReservationRepository.java
@@ -1,2 +1,9 @@
-package kr.kro.airbob.domain.reservation.repository;public interface ReservationRepository {
+package kr.kro.airbob.domain.reservation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.kro.airbob.domain.reservation.entity.Reservation;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long>, ReservationRepositoryCustom{
+
 }
--- src/main/java/kr/kro/airbob/domain/reservation/repository/ReservationRepositoryCustom.java
+++ src/main/java/kr/kro/airbob/domain/reservation/repository/ReservationRepositoryCustom.java
@@ -1,2 +1,14 @@
-package kr.kro.airbob.domain.reservation.repository;public interface ReservationRepositoryCustom {
+package kr.kro.airbob.domain.reservation.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import kr.kro.airbob.domain.reservation.entity.Reservation;
+
+public interface ReservationRepositoryCustom {
+	boolean existsConflictingReservation(Long accommodationId, LocalDateTime checkIn, LocalDateTime checkOut);
+
+	boolean existsCompletedReservationByGuest(Long accommodationId, Long memberId);
+
+	List<Reservation> findFutureCompletedReservations(Long accommodationId);
 }
