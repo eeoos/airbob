b910d29e7fefa86fd80d287f7cab9e34d308be15
jaehoonchoi <eeoos8840@gmail.com>
2025-09-22 21:59:34 +0900

    feat: 예약 생성 api 로직 구현

--- src/main/java/kr/kro/airbob/domain/reservation/api/ReservationController.java
+++ src/main/java/kr/kro/airbob/domain/reservation/api/ReservationController.java
@@ -1,2 +1,35 @@
-package kr.kro.airbob.domain.reservation.api;public class ReservationController {
+package kr.kro.airbob.domain.reservation.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import kr.kro.airbob.domain.reservation.dto.ReservationRequest;
+import kr.kro.airbob.domain.reservation.dto.ReservationResponse;
+import kr.kro.airbob.domain.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reservations")
+public class ReservationController {
+
+	private final ReservationService reservationService;
+
+	@PostMapping
+	public ResponseEntity<ReservationResponse.Create> createReservation(
+		HttpServletRequest request,
+		@Valid @RequestBody ReservationRequest.Create requestDto) {
+
+		Long memberId = (Long) request.getAttribute("memberId");
+
+		ReservationResponse.Create response = reservationService.createPendingReservation(1L, requestDto);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+	}
 }
