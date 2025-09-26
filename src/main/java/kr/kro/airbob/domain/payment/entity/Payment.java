2b9f6c1e0ee28aacdd370e464a7cfbbcf5dffd6b
jaehoonchoi <eeoos8840@gmail.com>
2025-09-26 15:19:00 +0900

    feat: 결제 취소를 정의하는 PaymentCancel 엔티티 생성
    - Payment 엔티티와 양방향 관계 설

--- src/main/java/kr/kro/airbob/domain/payment/entity/Payment.java
+++ src/main/java/kr/kro/airbob/domain/payment/entity/Payment.java
@@ -1,12 +1,15 @@
 package kr.kro.airbob.domain.payment.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.Type;
 import org.hibernate.type.SqlTypes;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,6 +19,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import kr.kro.airbob.common.domain.BaseEntity;
@@ -62,6 +66,16 @@ public class Payment extends BaseEntity {
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "reservation_id", nullable = false)
 	private Reservation reservation;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private PaymentStatus status;
+
+	@Column(nullable = false)
+	private Long balanceAmount; // 잔액
+
+	@OneToMany(mappedBy = "payment", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<PaymentCancel> cancels = new ArrayList<>();
 
 	@PrePersist
 	protected void onCreate() {
--- src/main/java/kr/kro/airbob/domain/payment/entity/PaymentCancel.java
+++ src/main/java/kr/kro/airbob/domain/payment/entity/PaymentCancel.java
@@ -1,2 +1,46 @@
-package kr.kro.airbob.domain.payment.entity;public class PaymentCancel {
+package kr.kro.airbob.domain.payment.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.kro.airbob.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PaymentCancel extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "payment_id", nullable = false)
+	private Payment payment;
+
+	@Column(nullable = false)
+	private Long cancelAmount;
+
+	@Column(length = 200)
+	private String cancelReason;
+
+	@Column(nullable = false)
+	private String transactionKey;
+
+	@Column(nullable = false)
+	private LocalDateTime canceledAt;
 }
