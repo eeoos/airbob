32bd4824506975e513411c773f5925f07f2672a8
jaehoonchoi <eeoos8840@gmail.com>
2025-09-26 15:27:12 +0900

    refactor: payment method, status를 toss 공식문서에 맞게 변경

--- src/main/java/kr/kro/airbob/domain/payment/common/PaymentMethod.java
+++ src/main/java/kr/kro/airbob/domain/payment/common/PaymentMethod.java
@@ -1,12 +1,19 @@
 package kr.kro.airbob.domain.payment.common;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum PaymentMethod {
-	CREDIT_CARD,
-	DEBIT_CARD,
-	PAYPAL,
-	APPLE_PAY,
-	BANK_TRANSFER,
-	VIRTUAL_ACCOUNT,
-	POINTS,       // 적립금, 포인트
-	COUPON        // 전액 쿠폰 사용
+	CARD("카드"),
+	VIRTUAL_ACCOUNT("가상계좌"),
+	EASY_PAY("간편결제"),
+	MOBILE_PHONE("휴대폰"),
+	BANK_TRANSFER("계좌이체"),
+	CULTURE_GIFT_CARD("문화상품권"),
+	BOOK_GIFT_CARD("도서문화상품권"),
+	GAME_GIFT_CARD("게임문화상품권");
+
+	private final String description;
+
+
 }
