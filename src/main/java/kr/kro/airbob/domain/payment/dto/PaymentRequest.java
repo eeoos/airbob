2f050ab9e0445031df64edbf6e4eb14bcfc4152d
jaehoonchoi <eeoos8840@gmail.com>
2025-09-27 00:53:05 +0900

    feat: 결제 승인에 대한 요청 dto PaymentRequest.Confirm 생성

--- src/main/java/kr/kro/airbob/domain/payment/dto/PaymentRequest.java
+++ src/main/java/kr/kro/airbob/domain/payment/dto/PaymentRequest.java
@@ -1,2 +1,17 @@
-package kr.kro.airbob.domain.payment.dto;public class PaymentRequest {
+package kr.kro.airbob.domain.payment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PaymentRequest {
+
+	public record Confirm(
+		@NotBlank String paymentKey,
+		@NotBlank String orderId,
+		@NotNull @Positive Long amount
+	){}
 }
