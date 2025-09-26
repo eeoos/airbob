d9c01a5380f90e6e7875a87bafe75775e8d238b4
jaehoonchoi <eeoos8840@gmail.com>
2025-09-27 00:50:00 +0900

    feat: 예약과 결제를 분리시키고 PaymentController에서 이벤트를 발행하도록 변경

--- src/main/java/kr/kro/airbob/domain/payment/api/PaymentController.java
+++ src/main/java/kr/kro/airbob/domain/payment/api/PaymentController.java
@@ -1,2 +1,34 @@
-package kr.kro.airbob.domain.payment.api;public class PaymentController {
+package kr.kro.airbob.domain.payment.api;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import kr.kro.airbob.domain.payment.dto.PaymentRequest;
+import kr.kro.airbob.domain.reservation.event.ReservationEvent;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/payments")
+public class PaymentController {
+
+	private final ApplicationEventPublisher eventPublisher;
+
+	@PostMapping("/confirm")
+	public ResponseEntity<Void> confirmPayment(@Valid @RequestBody PaymentRequest.Confirm request) {
+		eventPublisher.publishEvent(
+			new ReservationEvent.ReservationPendingEvent(
+				request.amount().intValue(),
+				request.paymentKey(),
+				request.orderId()
+			)
+		);
+
+		return ResponseEntity.accepted().build();
+	}
 }
