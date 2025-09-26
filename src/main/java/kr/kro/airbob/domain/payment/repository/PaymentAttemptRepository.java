b6a6265465bc992b8068c5bb4a6701e2f4821b75
jaehoonchoi <eeoos8840@gmail.com>
2025-09-27 00:52:25 +0900

    feat: Payment, PaymentAttempt에 대한 repository 생성

--- src/main/java/kr/kro/airbob/domain/payment/repository/PaymentAttemptRepository.java
+++ src/main/java/kr/kro/airbob/domain/payment/repository/PaymentAttemptRepository.java
@@ -1,2 +1,8 @@
-package kr.kro.airbob.domain.payment.repository;public interface PaymentAttemptRepository {
+package kr.kro.airbob.domain.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.kro.airbob.domain.payment.entity.PaymentAttempt;
+
+public interface PaymentAttemptRepository  extends JpaRepository<PaymentAttempt, Long> {
 }
--- src/main/java/kr/kro/airbob/domain/payment/repository/PaymentRepository.java
+++ src/main/java/kr/kro/airbob/domain/payment/repository/PaymentRepository.java
@@ -1,2 +1,8 @@
-package kr.kro.airbob.domain.payment.repository;public interface PaymentRepository {
+package kr.kro.airbob.domain.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.kro.airbob.domain.payment.entity.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
