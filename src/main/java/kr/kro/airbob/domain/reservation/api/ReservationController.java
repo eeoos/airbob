40608d6453511679c86fd7d83bdd8dbce1ff9ffb
jaehoonchoi <eeoos8840@gmail.com>
2025-09-27 20:48:29 +0900

    feat: ReservationController에 결제 취소 api 구현

--- src/main/java/kr/kro/airbob/domain/reservation/api/ReservationController.java
+++ src/main/java/kr/kro/airbob/domain/reservation/api/ReservationController.java
@@ -1,15 +1,16 @@
 package kr.kro.airbob.domain.reservation.api;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import kr.kro.airbob.domain.payment.dto.PaymentRequest;
 import kr.kro.airbob.domain.reservation.dto.ReservationRequest;
 import kr.kro.airbob.domain.reservation.dto.ReservationResponse;
 import kr.kro.airbob.domain.reservation.service.ReservationService;
@@ -31,5 +32,14 @@ public class ReservationController {
 
 		ReservationResponse.Ready response = reservationService.createPendingReservation(1L, requestDto);
 		return ResponseEntity.ok(response);
+	}
+
+	@DeleteMapping("/{reservationUid}")
+	public ResponseEntity<Void> cancelReservation(
+		@PathVariable String reservationUid,
+		@Valid @RequestBody PaymentRequest.Cancel request
+	) {
+		reservationService.cancelReservation(reservationUid, request);
+		return ResponseEntity.accepted().build();
 	}
 }
