8e3f202be9a36fdc44221c4abbc1d4ac10c69ca1
jaehoonchoi <eeoos8840@gmail.com>
2026-01-12 19:20:28 +0900

    feat: health check를 위한 GET /health api 생성

--- src/main/java/kr/kro/airbob/health/HealthCheckController.java
+++ src/main/java/kr/kro/airbob/health/HealthCheckController.java
@@ -1,2 +1,16 @@
-package kr.kro.airbob.health;public class HealthCheckController {
+package kr.kro.airbob.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	@GetMapping("/health")
+	public ResponseEntity<String> health(){
+		return ResponseEntity.ok("OK");
+	}
+
+
 }
